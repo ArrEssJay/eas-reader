2fae915903f17fcd7c9075f17752292ce8f47078
Rowan Jones <rowan@impermanent.io>
2025-03-15 13:18:05 +1100

    improve parsing

--- eas-reader.py
+++ eas-reader.py
@@ -15,12 +15,8 @@ import os, base64, json, sys
 NO_WRITE = False
 
 # Load brigade lookup from CSV
-def load_brigades_lookup(csv_file="data/cfa_brigades.csv"):
-    """
-    Load brigades lookup from a CSV file with columns:
-    distrct_no, brig_no, brig_name, short_name, grp_name.
-    Uses 'short_name' as the key.
-    """
+def load_stations_lookup(csv_file="data/cfa_frv_stations.csv"):
+
     lookup = {}
     with open(csv_file, "r", newline="") as csvfile:
         reader = csv.DictReader(csvfile)
@@ -101,91 +97,156 @@ def create_log_entry(parsed_message):
 def parse_message(data):
     """Parse the EAS message and extract relevant fields."""
     parsed_message = {}
-    message = data['message']
-    parsed_message['message'] = message
+    message_body= data['message']
+    parsed_message['message'] = message_body
 
     # Extract EAS priority
-    if re.match(r"@@", message):
+    if re.match(r"@@", message_body):
         parsed_message['eas_priority'] = "EMERGENCY"
-        parsed_message['message'] = re.sub(r"@@", "", message)
-    elif re.match(r"Hb", message):
+    elif re.match(r"Hb", message_body):
         parsed_message['eas_priority'] = "NON_EMERGENCY"
-        parsed_message['message'] = re.sub(r"Hb", "", message)
-    elif re.match(r"QD", message):
+    elif re.match(r"QD", message_body):
         parsed_message['eas_priority'] = "ADMIN"
-        parsed_message['message'] = re.sub(r"QD", "", message)
+
+    message_body = re.sub(r"@@|Hb|QD", "", message_body)
+    #parsed_message['message'] = message_body
 
     # Extract address and address range
     capcode = int(data['address'])
-    parsed_message['capcode'] = data['address']
+    #parsed_message['capcode'] = data['address']
 
     if capcode % 8 == 0:
         parsed_message['address'] = capcode >> 3
         parsed_message['address_range'] = "CFA"
-        parse_cfa_message(parsed_message)
     elif  (capcode -1) % 8 == 0:
         parsed_message['address'] = (capcode-1) >> 3
         parsed_message['address_range'] = "SES"
     else:
-        parsed_message['address'] = -1
+        parsed_message['address'] = capcode
         parsed_message['address_range'] = "Unknown"
     
-    return parsed_message
 
 
-    
+    #Fire call matching is somewhat tricky as pages can be sent manually, and there may
+    #be followup messages. Generally we're looking for something sent high priority with a job number
+    #to a group capcode
 
-def parse_cfa_message(parsed_message):
+    parsed_data = {}
 
-    #Fire call
-    alert = re.search(r"ALERT ", parsed_message['message'])
+    # Alert
+    alert = re.match(r"ALERT", message_body)
     if alert:
-        parsed_message['f_alert'] = True
+        parsed_message['alert'] = True
+        message_body= message_body.replace(alert.group(), "").lstrip()
 
-        #Area 
-        f_area = re.search(r"[A-Z]{4,6}[0-9]{1,2}[A-Z]{0,1}|[0-9]{5}[A-Z]?", parsed_message['message'])
-        if f_area:
-         parsed_message['f_area'] = f_area.group()
+    # Area code will follow alert
+    area = re.match(r"[A-Z]{4,6}[0-9]{1,2}[A-Z]{0,1}|[0-9]{5}[A-Z]?", message_body)
+    if area:
+        message_body= message_body.replace(area.group(), "").lstrip()
+        parsed_message['area'] = area.group()
     
-        # Extract event type and code
-        event_type_match = re.search(
-            r"(G&S|RESC|ALAR|INCI|NOST|NS&R|STRU|TRCH|CONF|STCO|MINE|HIAR)C(\d+)",
-            parsed_message['message']
-        )
-        if event_type_match:
-            event_type_coded = event_type_match.group(1)
-        
-            parsed_message['f_event_type'] = event_descriptions.get(event_type_coded, "Unknown")
-            parsed_message['f_code'] =  event_type_match.group(2)
+    # Extract event type and code
+    event_type_code = re.match(
+        r"(?P<event_type>G&S|RESC|ALAR|INCI|NOST|NS&R|STRU|TRCH|CONF|STCO|MINE|HIAR)C(?P<code>\d)", message_body
+    )
+    
+    if event_type_code:
+        message_body= message_body.replace(event_type_code.group(), "").lstrip()
+        event_data = event_type_code.groupdict()
+        event_data['event_type_long'] = event_descriptions.get(event_data['event_type'], "")
+        parsed_data['event_type_code'] = event_data
 
-        # Extract Fireground channels
-        chans = re.findall(r"(?<=FGD)[0-9]{1,3}", parsed_message['message'])
-        # Remove duplicates and sort
-        if chans:
-            parsed_message['f_fgd_chans'] = ", ".join(dict.fromkeys(chans)) 
+    # Extract Fireground channels
+    fgd_chans = []
+    fgd_chans_iter = re.finditer(r"(FGD)([0-9]{1,3})", message_body)
+    
+    for match in fgd_chans_iter:
+        fgd_chans.append(match.group(2))
+        message_body= message_body.replace(match.group(), "").lstrip()
+    
+    parsed_data['fgd_chans'] = fgd_chans
 
 
-        # Extract ESTA job ID
-        job_id_match = re.search(r"F[0-9]{9}", parsed_message['message'])
-        if job_id_match:
-            parsed_message['f_job_id'] = job_id_match.group()
+    # Extract ESTA job ID
+    job_ids = re.finditer(r"(?P<job_type>F|S|E)(?P<job_num>[0-9]{9})", message_body)
+    jobs = [j.group() for j in job_ids]
 
+    if jobs:
+        parsed_data['job_ids'] = jobs
+        for j in jobs:
+            message_body= message_body.replace(j, "").lstrip()
 
 
     # Extract paged group and lookup if present
-    paged_group = re.search(r"(\[)([A-Z,0-9,_]{4,6})(\])", parsed_message['message'])
+    # CFA/FRV share an address space so we have to manually lookup
+    paged_group = re.search(r"(\[)([A-Z,0-9,_]{4,6})(\])", message_body)
     if paged_group:
-        parsed_message['f_paged'] = paged_group.group(2)
-        brigade = brigades_lookup.get(parsed_message['f_paged'])
-        if brigade:
-            parsed_message['f_distrct_no'] = brigade.get("distrct_no", "Unknown")
-            parsed_message['f_brig_name'] = brigade.get("brig_name", "Unknown")
-        else:
-            parsed_message['f_distrct_no'] = "Unknown"
-            parsed_message['f_brig_name'] = "Unknown"
-    else:
-        parsed_message['f_paged'] = "Unknown"
+        message_body= message_body.replace(paged_group.group(), "").lstrip()
+        # remove underscores from paged group
+        parsed_message['paged'] = paged_group.group(2).replace("_", "")
+        paged_lookup = brigades_lookup.get( paged_group.group(2))
 
+        if paged_lookup: 
+            parsed_message['district'] = paged_lookup.get("district", "")
+            parsed_message['paged_name'] = paged_lookup.get("name", "")
+            parsed_message['org'] = paged_lookup.get("org", "")
+
+    # SES don't share an address space
+    if parsed_message['address_range'] == "SES":
+        parsed_message['org'] = 'SES'
+    
+    # Resources paged
+    # After the map ref (XXXXXX) <agencies> <resources>
+    # agencies is optional and not sent in soem resource request messages
+    agencies_resources = re.search(r"(?<=\([0-9]{6}\))(?:.)(\* (?P<advice>.+) \*)?\s?(?P<agencies>(?:A|F|P|EM|R)+ )?(?P<resources>.*)", message_body)
+    if agencies_resources:
+        # agencies
+        if agencies_resources.group('agencies'):
+            message_body= message_body.replace(agencies_resources.group('agencies'), "").lstrip()
+            agencies = agencies_resources.group('agencies').strip()
+            agency_list = []
+            if "A" in agencies:
+                agency_list.append("Ambulance")
+            if "F" in agencies:
+                agency_list.append("Fire")
+            if "P" in agencies:
+                agency_list.append("Police")
+            if "EM" in agencies:
+                agency_list.append("Emergency Medical Response")
+            if "R" in agencies:
+                agency_list.append("Rescue")
+            parsed_data['agencies'] = agency_list
+
+        # resources
+        message_body= message_body.replace(agencies_resources.group('resources'), "").lstrip()
+        resources = agencies_resources.group('resources').split()
+
+        resource_dict = {'CFA_BRIGADE': [], 'FRV': [], 'other': [] }
+        for r in resources:
+            if re.match(r"C[A-Z]{4}", r):
+                resource_dict['CFA_BRIGADE'].append(r[1:])
+            elif r.startswith("P"):
+                resource_dict['FRV'].append(r)
+            else:
+                resource_dict['other'].append(r)
+        parsed_data['resources'] = resource_dict
+
+        #advice
+        if agencies_resources.group('advice'):
+            message_body= message_body.replace(agencies_resources.group('advice'), "").lstrip()
+            parsed_data['advice'] = agencies_resources.group('advice')
+
+    # Extract book + page + grid refs
+    location = re.search(r"(?P<book>SV[A-Z]{2}|M) (?P<page>\S+) (?P<square>\S+) \((?P<grid_ref>[0-9]{6})\)", message_body)
+    if location:
+        message_body= message_body.replace(location.group(), "").lstrip()
+        parsed_data['location'] = location.groupdict()
+   
+    # Trim whitespace
+    parsed_message['message_body'] = " ".join(message_body.split())
+
+    # Add additional parsed data json
+    parsed_message['parsed_data'] = json.dumps(parsed_data)
     return parsed_message
 
 def start_client(server_url, shutdown_event):
@@ -237,7 +298,11 @@ def main():
                         help="Disable writing logs to Loki.")
     parser.add_argument("-c", "--config", default="config.json",
                         help="Path to configuration file (default: config.json)")
+    parser.add_argument("--test", action="store_true",
+                        help="Enable test mode to read messages from test_msgs.json.")
+  
     args = parser.parse_args()
+
     # If the BASE64_CONFIG environment variable is set, decode and parse it as JSON.
     if os.environ.get("BASE64_CONFIG"):
         print("Using BASE64_CONFIG environment variable for configuration.", file=sys.stderr)
@@ -259,7 +324,38 @@ def main():
     LOKI_PASSWORD = config["loki_password"]
 
     # Reload the brigade lookup in case paths or data have changed
-    brigades_lookup = load_brigades_lookup()
+    brigades_lookup = load_stations_lookup()
+
+    if args.test:
+        print("Running in test mode...", file=sys.stderr)
+        try:
+            with open("test/test_msgs.json", "r") as f:
+                test_data = json.load(f)
+                messages = test_data["messages"]
+                processed_messages = []
+                for msg in messages: 
+                    processed_messages.append(parse_message(msg))
+                #fieldnames =  set().union(*(d.keys() for d in processed_messages))
+                fieldnames = [
+                    'address', 'eas_priority', 'message', 'paged', 'org', 'paged_name', 'parsed_data',
+                              'district',
+                                'address_range', 'alert',  'area', 'message_body'
+                              ]
+                writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerows(processed_messages)
+                
+
+        except FileNotFoundError:
+            print("Error: test_msgs.json not found in the test directory.", file=sys.stderr)
+            sys.exit(1)
+        except Exception as e:
+            print(f"Error processing test messages: {e}", file=sys.stderr)
+            import traceback  # Import the traceback module
+            traceback.print_exc()  # Print the full traceback
+            sys.exit(1)
+        return  # Exit after test mode
+
 
     threads = []
     for server_url in SOCKETIO_SERVERS:
